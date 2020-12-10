1e0add7a264f026f0f9097465f3b3ffd086d58da
james McBride <james.m.mcbride15@gmail.com>
2020-12-10 16:57:00 -0600

    deleting keys file to be replaced with new key

--- js/keys.js
+++
@@ -1,3 +0,0 @@
-//this file is for saving API keys and not uploading to Github
-
-const mapBoxToken = 'pk.eyJ1IjoiamFtZXMtbWNicmlkZSIsImEiOiJja2lqMHhudGEwdmtyMnJsY2VodHpkdmE1In0.q2A-peliF2vmbST01Es9TA';
