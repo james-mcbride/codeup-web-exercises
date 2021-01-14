a2715ca662bf8cade60634037527d786186d412f
james McBride <james.m.mcbride15@gmail.com>
2021-01-14 17:47:57 -0600

    modified promises exercise to work properly after lecture

--- js/promises.js
+++ js/promises.js
@@ -1,27 +1,32 @@
-function usernameCommits (username){
-    return fetch("https://api.github.com/users/{username}/events/public", {headers: {'Authorization': 'token 1e08ec360fae644562b2ba618d5a145990e3df87'}},{
-        username: username
-    })
-        .then(response=>console.log(response))
-        .catch(error=> console.log("Something went wrong" + error))
+
+const options= {
+    headers: {'Authorization': `token ${githubToken}`},
 }
 
-console.log(usernameCommits("james-mcbride"));
+function getUserActivity (username) {
+    fetch(`https://api.github.com/users/${username}/events/public`, options)
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data)
+            console.log(data[0].created_at)
+        })
+        .catch(error => console.log(error))
+}
+getUserActivity("james-mcbride")
 
 
-//wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
-// function wait(time){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if (Math.random() > 0.1) {
-//                 resolve(console.log("You'll see this after ${time} seconds"));
-//             } else {
-//                 reject(console.log('Network Connection Error!'));
-//             }
-//         }, time);
-//     })
-// }
-// wait(1000)
+function wait(time){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.1) {
+                resolve(`You'll see this after ${time} milliseconds unless we have some bad luck!`);
+            } else {
+                reject('Shoot some bad luck occurred!');
+            }
+        }, time);
+    })
+}
+wait(1000).then(message => console.log(message));
+wait(3000).then(message => console.log(message));
 
